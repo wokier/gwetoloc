0f2b637b51c7f558a3d3ccd89aec7865372a83c0
wokier <wokier@4d521fee-7d3b-ede8-f791-03c3b7f8c2c3>
2011-08-06 08:27:50 +0000

    0.0.1-beta

---
+++ gwt-geolocation/src/main/java/com/googlecode/gwetoloc/model/Coordinates.java
@@ -0,0 +1,119 @@
+package com.googlecode.gwetoloc.model;
+
+import java.io.Serializable;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * http://dev.w3.org/geo/api/spec-source.html#coordinates
+ * 
+ * @author Francois 'wokier' Wauquier
+ * 
+ */
+public class Coordinates implements Serializable, IsSerializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private double latitude;
+    private double longitude;
+    private double accuracy;
+
+    private Double altitude;
+    private Double altitudeAccuracy;
+
+    private Double heading;
+    private Double speed;
+
+    /**
+     * Constructor
+     */
+    public Coordinates() {
+	super();
+    }
+
+    /**
+     * Constructor with required fields
+     * 
+     * @param latitude
+     * @param longitude
+     * @param accuracy
+     */
+    public Coordinates(double latitude, double longitude, double accuracy) {
+	super();
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.accuracy = accuracy;
+    }
+
+    @Override
+    public String toString() {
+	return super.toString() + ":" + latitude + " " + longitude + " " + accuracy;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+	return super.hashCode();
+    }
+
+    public double getLatitude() {
+	return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+	this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+	return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+	this.longitude = longitude;
+    }
+
+    public double getAccuracy() {
+	return accuracy;
+    }
+
+    public void setAccuracy(double accuracy) {
+	this.accuracy = accuracy;
+    }
+
+    public Double getAltitude() {
+	return altitude;
+    }
+
+    public void setAltitude(Double altitude) {
+	this.altitude = altitude;
+    }
+
+    public double getAltitudeAccuracy() {
+	return altitudeAccuracy;
+    }
+
+    public void setAltitudeAccuracy(Double altitudeAccuracy) {
+	this.altitudeAccuracy = altitudeAccuracy;
+    }
+
+    public Double getHeading() {
+	return heading;
+    }
+
+    public void setHeading(Double heading) {
+	this.heading = heading;
+    }
+
+    public Double getSpeed() {
+	return speed;
+    }
+
+    public void setSpeed(Double speed) {
+	this.speed = speed;
+    }
+
+}
