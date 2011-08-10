4f643286ea1cf4e16c9871a61d5f74a872952bca
wokier <wokier@4d521fee-7d3b-ede8-f791-03c3b7f8c2c3>
2011-08-10 01:28:45 +0000

    Error Handling

--- gwt-geolocation/src/main/java/com/googlecode/gwetoloc/geo/NavigatorGeolocation.java
+++ gwt-geolocation/src/main/java/com/googlecode/gwetoloc/geo/NavigatorGeolocation.java
@@ -1,11 +1,11 @@
 package com.googlecode.gwetoloc.geo;
 
-import com.googlecode.gwetoloc.model.PositionOptions;
-
 /**
- * GWeToloc<br>
- * http://dev.w3.org/geo/api/spec-source.html#introduction
+ * GWeToloc HTML5 geolocation<br>
  * 
+ * @see <a href="http://www.w3.org/TR/geolocation-API/">W3C Geolocation API</a>
+ * @see <a href="http://diveintohtml5.org/geolocation.html">Dive Into HTML5 -
+ *      Geolocation</a>
  * @author Francois 'wokier' Wauquier
  */
 public class NavigatorGeolocation {
@@ -41,22 +41,27 @@ public class NavigatorGeolocation {
     }
 
     private static native boolean getCurrentPositionHtml5(PositionCallback callback, PositionOptions options)/*-{
-
-													     if (navigator.geolocation) {
-													     navigator.geolocation.getCurrentPosition(function(position){
-													     var gwtCoordinates = @com.googlecode.gwetoloc.model.Coordinates::new(DDD)(position.coords.latitude, position.coords.longitude, position.coords.accuracy);
-													     var gwtPosition = @com.googlecode.gwetoloc.model.Position::new(Lcom/googlecode/gwetoloc/model/Coordinates;D)(gwtCoordinates,position.timestamp);
-													     callback.@com.googlecode.gwetoloc.geo.PositionCallback::onSuccess(Lcom/googlecode/gwetoloc/model/Position;)(gwtPosition);
-													     });
-													     return true;
-													     }else{
-													     return false;
-													     }
-													     }-*/;
+		if (navigator.geolocation) {
+			var opt = options.@com.googlecode.gwetoloc.geo.PositionOptions::toJso()();
+			navigator.geolocation
+					.getCurrentPosition(
+							function(position) {
+								var gwtCoordinates = @com.googlecode.gwetoloc.model.Coordinates::new(DDD)(position.coords.latitude, position.coords.longitude, position.coords.accuracy);
+								var gwtPosition = @com.googlecode.gwetoloc.model.Position::new(Lcom/googlecode/gwetoloc/model/Coordinates;D)(gwtCoordinates,position.timestamp);
+								callback.@com.googlecode.gwetoloc.geo.PositionCallback::onSuccess(Lcom/googlecode/gwetoloc/model/Position;)(gwtPosition);
+							},
+							function(error) {
+								callback.@com.googlecode.gwetoloc.geo.PositionCallback::onError(ILjava/lang/String;)(error.code, error.message);
+							}, opt);
+			return true;
+		} else {
+			return false;
+		}
+    }-*/;
 
     private static native boolean getCurrentPositionGears(PositionCallback callback)/*-{
 
-										    return false;
-										    }-*/;
+		return false;
+    }-*/;
 
 }
